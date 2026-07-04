5bd197c28b0fff90205dbcf9da1710910c2c8784
chamodhlv <chamodh300@gmail.com>
2026-07-04 23:06:38 +0530

    fix: correct shortUrl field name to shortId in Url model feat: implement URL shortening logic in urlRoutes

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,5 +1,32 @@
 import express from "express";
 import Url from "../models/Url.js";
+import cors from "cors";
 import { nanoid } from "nanoid";
 
+dotenv.config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
+
 const router = express.Router();
+
+router.post("/shorten", async (req, res) => {
+  try {
+    const { originalUrl } = req.body;
+
+    if (!originalUrl) {
+      return res.status(400).json({ error: "Original URL is required" });
+    }
+
+    try {
+      new URL(originalUrl);
+    } catch {
+      return res.status(400).json({ error: "Invalid URL format" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
